bc230b9093c5ecde21f46c9a53307f5f96a861d6
Silvia Lin Lü <ssll@kth.se>
2026-01-19 12:45:31 +0100

    Create aes.c

---
+++ aes.c
@@ -0,0 +1,16 @@
+/* 
+Output HEX:
+52E418CBB1BE494930203938162018B109FE
+
+Input HEX:
+F4C020A0A1F604FD343FAC6A7E6AE0F9 -- key
+F22022B931203921224434D93D2DCA4E449AFD8 -- block to encrypt
+*/
+
+/* Input format: 
+128 bits, 16 bytes -- key (fixed size 128, other too?)
+blocks to encode -- 16 bytes
+output -- each encrypted block, 128 bits, 16 bytes
+ */
+
+ 
