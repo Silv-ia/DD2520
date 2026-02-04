597d0525b7bef12154f0b98908d18456390e0ece
Silvia Lin Lü <ssll@kth.se>
2026-02-04 18:45:25 +0100

    edit

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,18 +1,10 @@
 from collections import Counter
 import operator
 
-cipherText = "ksjdiskdlekfabcdefghijklmnopqrstuvwxyzl"
+cipherText = "oxmanaosnflsåwöwkdölsnajfksjdiskdlekfabcdefghijklmnopqrstuvwxyzl"
 
 cipher = Counter(cipherText)
 print(cipher)
-
-for ci in cipher:
-    cipher[ci] /= len(cipher)
-
-print(cipher)
-
-# sorted_freq = sorted(cipher.items(), key=operator.itemgetter(1), reverse=True)
-# print(sorted_freq)
 
 print(cipher.keys())
 
--- shift.py
+++ shift.py
@@ -8,4 +8,4 @@
 cipher = ""
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-
+# [index+1] -- to find the corresponding. plain+key % 26 - 1, something. then decr, 
