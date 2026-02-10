74e07cb3c5ab971ddcfa332f3643c682cd393e1c
Silvia Lin Lü <ssll@kth.se>
2026-02-10 17:14:22 +0100

    Create decipher.py

---
+++ decipher.py
@@ -0,0 +1,14 @@
+cipher = "pmaolylpzhufaopunavillujyfwalkaoludlzovbskuvailbzpunhjhlzhyjpwoly"
+key = 7
+
+alphabet=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+          'q','r','s','t','u','v','w','x','y','z')
+
+def decrypt(key, cipher):
+    plaintext = ""  
+
+    for c in cipher:
+        plaintext += alphabet[(alphabet.index(c)-key) % 26]
+    return plaintext
+
+print(decrypt(key, cipher))
